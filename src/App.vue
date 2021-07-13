7a5dc512b1031dcc3d346b98ea0ba0c044c02e61
Dmitry <DmitryVoln@mail.ru>
2021-07-13 17:13:56 +0300

    task3

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,56 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h3>My App</h3>
+    <add-payment v-on:addNewPayment="addData"/>
+    <br/>
+    <paymentsDisplay v-bind:list="paymentsList"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import PaymentsDisplay from './components/PaymentsDisplay.vue';
+import addPayment from './components/addPayment.vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PaymentsDisplay,
+    addPayment,
+  },
+  data: () => ({
+    paymentsList: []
+  }),
+  methods: {
+    fetchData() {
+      return [
+        {
+          date: '13.07.21',
+          category: 'sport',
+          cost: 200,
+        },
+        {
+          date: '13.07.21',
+          category: 'food',
+          cost: 150,
+        },
+        {
+          date: '13.07.21',
+          category: 'hobby',
+          cost: 130,
+        },
+      ]
+    },
+    addData(data) {
+      this.paymentsList.push(data);
+    }
+  },  //created hook:
+  created() {
+    this.paymentsList = this.fetchData()
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+ 
+  }
 </style>
